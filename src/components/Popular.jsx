ecb113292690337aa7fe5e670ce37b6112840dbc
Natalie Gillam <ngillam@hotmail.co.uk>
2023-08-08 23:19:13 +0100

    mapped over recipes returned from api to show title, image and summary

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useEffect, useState } from "react";
 
 const Popular = () => {
-    // const [popular, setPopular] = useEffect();
+  const [popular, setPopular] = useState([]);
 
   useEffect(() => {
     getPopular();
@@ -11,12 +11,25 @@ const Popular = () => {
   const getPopular = async () => {
     // const api = await fetch(`https://api.spoonacular.com/recipes/random?number=3&limitLicense=true`)
     const api = await fetch(
-      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=3&limitLicense&glutenFree=true&dairyFree=true`
+      `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=2&limitLicense`
     );
     const data = await api.json();
     console.log(data);
+    setPopular(data.recipes);
   };
-  return <div>Popular</div>;
+  return (
+    <div>
+      {popular.map((recipe) => {
+        return (
+          <div key={recipe.id}>
+            <h2>{recipe.title}</h2>
+            <img src={recipe.image} alt={recipe.title} />
+            <p>{recipe.summary}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Popular;
